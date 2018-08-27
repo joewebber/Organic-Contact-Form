62d1c2a35f94bce82a2dbf9029d8563fb6828eca
Joe Webber <signup@joewebber.co.uk>
2018-08-27 18:00:38 +0100

    Admin Dashboard view

--- admin/partials/organic-contact-form-dashboard.php
+++ admin/partials/organic-contact-form-dashboard.php
@@ -1,3 +1,18 @@
+<?php
+
+/**
+ * The dashboard view
+ *
+ * This file is used to markup the HTML for the dashboard
+ *
+ * @link       http://joewebber.co.uk
+ * @since      1.0.0
+ *
+ * @package    Organic_Contact_Form
+ * @subpackage Organic_Contact_Form/admin/partials
+ */
+?>
+
 <div class="wrap organic-contact-form">
 
 	<header>
@@ -5,5 +20,43 @@
 		<h1><?php echo $this->plugin_title; ?></h1>
 
 	</header>
+
+	<div class="metabox-holder">
+
+		<div class="postbox-container full-width">
+
+			<div class="postbox">
+
+				<h2 class="hndle"><span>Recent Submissions</span></h2>
+
+			</div>
+
+		</div>
+
+	</div>
+
+	<div class="metabox-holder">
+
+		<div class="postbox-container half-width">
+
+			<div class="postbox">
+
+				<h2 class="hndle"><span>Top Pages</span></h2>
+
+			</div>
+
+		</div>
+
+		<div class="postbox-container half-width">
+
+			<div class="postbox">
+
+				<h2 class="hndle"><span>Top Days</span></h2>
+
+			</div>
+
+		</div>
+
+	</div>
 
 </div>
