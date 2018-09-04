04efb43e41830b0811049df386b6f9e2c49012c7
Joe Webber <signup@joewebber.co.uk>
2018-09-04 16:16:58 +0100

    Fix gulp compilation not watching in gulp v4

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Require gulp
 var gulp = require('gulp');
 
@@ -8,35 +10,51 @@ var sass = require('gulp-sass');
 var minify = require('gulp-minify');
 
 // Gulp task to minify admin js
-gulp.task('minify-admin-js', function() {
+gulp.task('minify-admin-js', function(done) {
   gulp.src(['admin/js/*.js'])
     .pipe(minify({ignoreFiles: ['*-min.js']}))
-    .pipe(gulp.dest('./admin/js/'))
+    .pipe(gulp.dest('./admin/js/'));
+
+    // Run the callback function
+    done();
+
 });
 
 // Gulp task to minify public js
-gulp.task('minify-public-js', function() {
+gulp.task('minify-public-js', function(done) {
   gulp.src(['public/js/*.js'])
     .pipe(minify({ignoreFiles: ['*-min.js']}))
-    .pipe(gulp.dest('./public/js/'))
+    .pipe(gulp.dest('./public/js/'));
+
+    // Run the callback function
+    done();
+
 });
 
 // Gulp task for admin styles
-gulp.task('admin-sass', function() {
+gulp.task('admin-sass', function(done) {
     gulp.src('admin/sass/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./admin/css/'));
+        
+    // Run the callback function
+    done();
+
 });
 
 // Gulp task for public styles
-gulp.task('public-sass', function() {
+gulp.task('public-sass', function(done) {
     gulp.src('public/sass/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./public/css/'));
+
+    // Run the callback function
+    done();
+
 });
 
 // Watch for changes to files
-gulp.task('default',function() {
+gulp.task('default',function(done) {
 
 	// Watch admin files for sass changes
     gulp.watch('admin/sass/*.scss', gulp.series('admin-sass'));
@@ -49,5 +67,8 @@ gulp.task('default',function() {
 
     // Watch public files for js changes
     gulp.watch('public/js/*.js', gulp.series('minify-public-js'));
+
+    // Run the callback function
+    done();
 
 });
